9f57b02f5d94df8d327d6c8515df4ecb7bd70887
Mateusz <mwwyoghurt@gmail.com>
2025-04-23 18:49:12 +0200

    changing solution structure. Added class library project and api project

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,33 +1,23 @@
-using Backend.Hubs;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSignalR();
+// Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-app.MapControllers();
-
-app.UseRouting();
-
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapHub<BridgeHub>("/gamehub");
-});
+app.MapControllers();
 
 app.Run();
