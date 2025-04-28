a67bdf37365c16e595cfe438a6e665345666ad71
Mateusz <mwwyoghurt@gmail.com>
2025-04-28 12:50:02 +0200

    user service added

---
+++ Backend/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace Backend.Services;
+
+public class UserService : IUserService
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public UserService(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public string GetCurrentUserId()
+    {
+        return httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+    }
+}
