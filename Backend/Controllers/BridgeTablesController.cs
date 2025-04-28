5490e1e1bd1466b2363ccc280bcf45eff467c0b7
Mateusz <mwwyoghurt@gmail.com>
2025-04-28 12:52:23 +0200

    BridgeTablesController added

---
+++ Backend/Controllers/BridgeTablesController.cs
@@ -0,0 +1,69 @@
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace Backend.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/bridge-tables")]
+public class BridgeTableController : ControllerBase
+{
+    private readonly IBridgeTablesService bridgeTablesService;
+
+    public BridgeTableController(IBridgeTablesService bridgeTablesService)
+    {
+        this.bridgeTablesService = bridgeTablesService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BridgeTable>>> GetAllBridgeTables()
+    {
+        List<BridgeTable> tables = await bridgeTablesService.GetAllBridgeTablesAsync();
+
+        return Ok(tables);
+    }
+
+    [HttpGet("{bridgeTableId:long}")]
+    public async Task<ActionResult<BridgeTable>> GetBridgeTable(int bridgeTableId)
+    {
+        BridgeTable table = await bridgeTablesService.GetBridgeTableByIdAsync(bridgeTableId);
+
+        return Ok(table);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<BridgeTable>> CreateBridgeTable([FromBody] int numberOfDeals)
+    {
+        BridgeTable createdTable = await bridgeTablesService.CreateBridgeTableAsync(numberOfDeals);
+
+        var url = Url.Action("api/bridge-tables", new { bridgeTableId = createdTable.Id });
+
+        return Created(url, createdTable);
+    }
+
+    [HttpPatch("{bridgeTableId:long}/add-user/{userId:string}")]
+    public async Task<ActionResult<BridgeTable>> AddUserToBridgeTable(long bridgeTableId, string userId)
+    {
+        await bridgeTablesService.AddUserToBridgeTableAsync(bridgeTableId, userId);
+
+        return Ok();
+    }
+
+    [HttpPatch("{bridgeTableId:long}/remove-user/{userId:string}")]
+    public async Task<ActionResult<BridgeTable>> RemoveUserFromBridgeTable(long bridgeTableId, string userId)
+    {
+        await bridgeTablesService.RemoveUserFromBridgeTableAsync(bridgeTableId, userId);
+
+        return Ok();
+    }
+
+    [HttpDelete("{bridgeTableId:long}")]
+    public async Task<IActionResult> DeleteBridgeTable(long bridgeTableId)
+    {
+        await bridgeTablesService.DeleteBridgeTableAsync(bridgeTableId);
+
+        return NoContent();
+    }
+}
