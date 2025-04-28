eb51d228717aebc917e7c969a93ebda7699a7f55
Mateusz <mwwyoghurt@gmail.com>
2025-04-28 12:49:32 +0200

    bridge table model added

---
+++ Shared/BridgeTable.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared;
+
+public class BridgeTable
+{
+    public long? Id { get; set; }
+    public string AdminId {  get; set; }
+    public List<string> PlayersIds { get; set; }
+    public int NumberOfDeals { get; set; }
+}
