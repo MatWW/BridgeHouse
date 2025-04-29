afa06a5427ff662ca826b2c67657b84b688a76e2
Mateusz <mwwyoghurt@gmail.com>
2025-04-29 14:02:02 +0200

    GameState model added

---
+++ Shared/GameState.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Enums;
+
+namespace Shared;
+
+public class GameState
+{
+    public long Id { get; set; }
+    public List<PlayerInfo> Players { get; set; } = new();
+    public GamePhase GamePhase { get; set; }
+    public Dictionary<string, List<Card>> PlayerHands { get; set; } = new();
+    public BiddingState BiddingState { get; set; } = new();
+    public PlayingState PlayingState { get; set; } = new();
+}
