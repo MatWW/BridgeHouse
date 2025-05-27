209346d5d8937a4adfc598a1e16e768b7c88abe9
Mateusz <mwwyoghurt@gmail.com>
2025-05-27 18:31:38 +0200

    CookieHandler to frontend added

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,4 +1,5 @@
 using Frontend;
+using Frontend.CookieHandler;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7200/") });
+builder.Services.AddScoped(sp => new HttpClient(new CookieHandler())
+{
+    BaseAddress = new Uri("https://localhost:7200/") 
+});
+
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Add("Cookie", "test=value");
 
 await builder.Build().RunAsync();
