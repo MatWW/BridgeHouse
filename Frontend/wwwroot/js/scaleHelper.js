0ac14ac7c04495e0c3182b2adb47894c8eff8d93
Mateusz <mwwyoghurt@gmail.com>
2025-05-27 18:57:52 +0200

    scale helpers added

---
+++ Frontend/wwwroot/js/scaleHelper.js
@@ -0,0 +1,16 @@
+﻿window.scaleHelper = {
+    updateScale: function () {
+        const baseWidth = 1416;
+        const baseHeight = 685;
+        const widthScale = window.innerWidth / baseWidth;
+        const heightScale = window.innerHeight / baseHeight;
+        document.documentElement.style.setProperty('--widthScale', widthScale);
+        document.documentElement.style.setProperty('--heightScale', heightScale)
+    },
+    initializeScaleListener: function () {
+        window.addEventListener('resize', () => this.updateScale());
+        this.updateScale();
+    }
+};
+
+window.scaleHelper.initializeScaleListener();
---
+++ Frontend/wwwroot/js/scaleHelper2.js
@@ -0,0 +1,16 @@
+﻿window.scaleHelper2 = {
+    updateScale: function () {
+        const baseWidth = 1920;
+        const baseHeight = 945;
+        const widthScale = window.innerWidth / baseWidth;
+        const heightScale = window.innerHeight / baseHeight;
+        document.documentElement.style.setProperty('--widthScale2', widthScale);
+        document.documentElement.style.setProperty('--heightScale2', heightScale)
+    },
+    initializeScaleListener: function () {
+        window.addEventListener('resize', () => this.updateScale());
+        this.updateScale();
+    }
+};
+
+window.scaleHelper2.initializeScaleListener();
