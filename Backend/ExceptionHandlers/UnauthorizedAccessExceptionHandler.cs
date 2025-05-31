a31566ca7ff3f4df0206a97e1e986c862168c5e2
Mateusz <mwwyoghurt@gmail.com>
2025-05-31 14:15:40 +0200

    UnauthorizedAccessExceptionHandler added

---
+++ Backend/ExceptionHandlers/UnauthorizedAccessExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.ExceptionHandlers;
+
+public class UnauthorizedAccessExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not UnauthorizedAccessException unauthorizedAccessException)
+        {
+            return false;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status403Forbidden,
+            Title = "Forbidden",
+            Detail = unauthorizedAccessException.Message
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
