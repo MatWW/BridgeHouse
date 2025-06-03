9eb66ffeb3d0bad5e7e7b42db11c6706e9bd0d29
Mateusz <mwwyoghurt@gmail.com>
2025-06-03 12:31:25 +0200

    GameController updated

--- Backend/Controllers/GameController.cs
+++ Backend/Controllers/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shared;
+using Shared.Enums;
 
 namespace Backend.Controllers;
 
@@ -8,118 +10,113 @@ namespace Backend.Controllers;
 [Route("api/game")]
 public class GameController : ControllerBase
 {
-    private readonly IGameService gameService;
-    private readonly IBiddingService biddingService;
-    private readonly IPlayingService playingService;
+    private readonly IGameService _gameService;
+    private readonly IBiddingService _biddingService;
+    private readonly IPlayingService _playingService;
 
     public GameController(IGameService gameService, IBiddingService biddingService, IPlayingService playingService)
     {
-        this.gameService = gameService;
-        this.biddingService = biddingService;
-        this.playingService = playingService;
+        _gameService = gameService;
+        _biddingService = biddingService;
+        _playingService = playingService;
     }
 
-    [HttpGet("{gameId:long}/gameState")]
-    public async Task<ActionResult<GameState>> GetGameState(long gameId)
-    {
-        var gameState = await gameService.GetGameStateAsync(gameId);
-
-        return Ok(gameState);
-    }
-
+    [Authorize]
     [HttpGet("{gameId:long}/biddingState")]
     public async Task<ActionResult<BiddingState>> GetBiddingState(long gameId)
     {
-        var biddingState = await gameService.GetBiddingStateAsync(gameId);
+        var biddingState = await _gameService.GetBiddingStateAsync(gameId);
 
         return Ok(biddingState);
     }
 
+    [Authorize]
     [HttpGet("{gameId:long}/playingState")]
     public async Task<ActionResult<PlayingState>> GetPlayingState(long gameId)
     {
-        var playingState = await gameService.GetPlayingStateAsync(gameId);
+        var playingState = await _gameService.GetPlayingStateAsync(gameId);
 
         return Ok(playingState);
     }
 
+    [Authorize]
     [HttpGet("{gameId:long}/contract")]
-    public async Task<ActionResult<PlayingState>> GetContractAsync(long gameId)
+    public async Task<ActionResult<Contract>> GetContractAsync(long gameId)
     {
-        var contract = await gameService.GetContractAsync(gameId);
+        var contract = await _gameService.GetContractAsync(gameId);
 
         return Ok(contract);
     }
 
-    [HttpGet("{gameId:long}/cards/{playerId}")]
-    public async Task<ActionResult<List<Card>>> GetPlayerCards(long gameId, string playerId)
+    [Authorize]
+    [HttpGet("{gameId:long}/cards/me")]
+    public async Task<ActionResult<List<Card>>> GetPlayerCards(long gameId)
     {
-        var playerCards = await gameService.GetPlayerCardsAsync(gameId, playerId);
+        var playerCards = await _gameService.GetSignedInPlayerCardsAsync(gameId);
 
         return Ok(playerCards);
     }
 
+    [Authorize]
     [HttpGet("{gameId:long}/cards/dummy")]
     public async Task<ActionResult<List<Card>>> GetDummiesCards(long gameId)
     {
-        var dummiesCards = await gameService.GetDummiesCardsAsync(gameId);
+        var dummiesCards = await _gameService.GetDummiesCardsAsync(gameId);
 
         return Ok(dummiesCards);
     }
 
-    [HttpGet("{gameId:long}/playerInfo/{playerId}")]
-    public async Task<ActionResult<Player>> GetPlayerInfo(long gameId, string playerId)
-    {
-        var playerInfo = await gameService.GetPlayerInfoAsync(gameId, playerId);
-
-        return Ok(playerInfo);
-    }
-
+    [Authorize]
     [HttpGet("{gameId:long}/playerInfo/current")]
     public async Task<ActionResult<Player>> GetCurrentPlayerInfo(long gameId)
     {
-        var playerInfo = await gameService.GetCurrentPlayerInfoAsync(gameId);
+        var playerInfo = await _gameService.GetCurrentPlayerInfoAsync(gameId);
 
         return Ok(playerInfo);
     }
 
+    [Authorize]
     [HttpGet("{gameId:long}/playerInfo/me")]
     public async Task<ActionResult<Player>> GetSignedInPlayerInfo(long gameId)
     {
-        var playerInfo = await gameService.GetSignedInPlayerInfoAsync(gameId);
+        var playerInfo = await _gameService.GetSignedInPlayerInfoAsync(gameId);
 
         return Ok(playerInfo);
     }
 
+    [Authorize]
+    [HttpGet("{gameId:long}/phase")]
+    public async Task<ActionResult<GamePhase>> GetGamePhase(long gameId)
+    {
+        var gamePhase = await _gameService.GetGamePhaseAsync(gameId);
+
+        return gamePhase;
+    }
+
+    [Authorize]
     [HttpPost("startGame")]
     public async Task<IActionResult> StartGame([FromBody] StartGameRequestDTO dto)
     {
-        await gameService.StartGameAsync(dto.tableId, dto.players);
+        await _gameService.StartGameAsync(dto.tableId, dto.players);
 
         return Created();
     }
 
+    [Authorize]
     [HttpPost("{gameId:long}/bid")]
     public async Task<IActionResult> PlaceBid(long gameId, [FromBody] BidAction bidAction)
     {
-        await biddingService.PlaceBidAsync(gameId, bidAction);
+        await _biddingService.PlaceBidAsync(gameId, bidAction);
 
         return Ok();
     }
 
+    [Authorize]
     [HttpPost("{gameId:long}/playCard")]
     public async Task<IActionResult> PlayCard(long gameId, [FromBody] CardPlayAction cardPlayAction)
     {
-        await playingService.PlayCardAsync(gameId, cardPlayAction);
+        await _playingService.PlayCardAsync(gameId, cardPlayAction);
 
         return Ok();
-    }
-
-    [HttpGet("me")]
-    public async Task<ActionResult<long?>> GetSignedInPlayerGameInfo()
-    {
-        long? gameId = await gameService.GetSignedInPlayerGameIdAsync();
-
-        return Ok(gameId);
     }
 }
