9a928e5e119397edcd85227f5de0f0d8c51a3871
Mateusz <mwwyoghurt@gmail.com>
2025-08-16 19:19:34 +0200

    add UserStatus enum

---
+++ Shared/Enums/UserStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum UserStatus
+{
+    IN_LOBBY,
+    INVITED,
+    AT_TABLE,
+    IN_GAME
+}
