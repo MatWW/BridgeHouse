a3e6c4813ded52cb5ccc2c9de334e47eecffc530
Mateusz <mwwyoghurt@gmail.com>
2025-08-16 19:20:18 +0200

    add UserStateDTO

---
+++ Shared/Models/UserStateDTO.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Enums;
+
+namespace Shared.Models;
+
+public class UserStateDTO
+{
+    public UserStatus UserStatus { get; set; }
+    public Invitation? Invitation { get; set; }
+    public long? TableId { get; set; }
+    public long? GameId { get; set; }
+}
