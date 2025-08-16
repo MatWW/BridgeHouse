db93ce1112ad77817bc9631ba65c3d9652093db8
Mateusz <mwwyoghurt@gmail.com>
2025-08-16 21:18:02 +0200

    add CreateInvitationDTO and AcceptInvitationDTO

---
+++ Shared/DTOs/CreateInvitationDTO.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs;
+
+public class CreateInvitationDTO
+{
+    [Required]
+    public required string UserId { get; set; }
+    [Required]
+    public Position? Position { get; set; }
+}
