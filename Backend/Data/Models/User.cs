98778a530574e0356a2aee5b4485064b5417caec
Mateusz <mwwyoghurt@gmail.com>
2025-08-17 23:06:59 +0200

    add User Model

---
+++ Backend/Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Data.Models;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Nickname { get; set; }
+    [Required]
+    public string PasswordHash { get; set; }
+}
