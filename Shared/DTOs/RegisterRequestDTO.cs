c2941a001e0fa81a6c3031e53b5024ce1396e917
Mateusz <mwwyoghurt@gmail.com>
2025-08-17 23:10:03 +0200

    add new LoginRequestDTO and RegisterRequestDTO and remove old ones

---
+++ Shared/DTOs/RegisterRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs;
+
+public class RegisterRequestDTO
+{
+    [Required]
+    public required string Email {  get; set; }
+    [Required]
+    public required string Nickname { get; set; }
+    [Required]
+    public required string Password { get; set; } 
+}
