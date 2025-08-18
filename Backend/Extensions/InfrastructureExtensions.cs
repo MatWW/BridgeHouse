f026e33d3770c85918ef822616c627b96e0b1c8d
Mateusz <mwwyoghurt@gmail.com>
2025-08-18 12:13:14 +0200

    add extension method adding db context and other singletons

---
+++ Backend/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Data.Models;
+using Backend.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+namespace Backend.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("MSSQL")));
+
+        services.AddSingleton<IConnectionMultiplexer>(_ =>
+            ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis")!));
+
+        services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+        services.AddSingleton<Random>();
+
+        return services;
+    }
+}
