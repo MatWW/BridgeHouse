0b96160a35e090adfbd2e9fc0670eb6fbd6ab2ba
Mateusz <mwwyoghurt@gmail.com>
2025-08-18 12:13:27 +0200

    add cors extension method

---
+++ Backend/Extensions/CorsExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Backend.Extensions;
+
+public static class CorsExtensions
+{
+    public static IServiceCollection AddCorsPolicies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowFrontend", policy =>
+            {
+                policy.WithOrigins(configuration["Frontend:Url"]!)
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials();
+            });
+        });
+
+        return services;
+    }
+}
